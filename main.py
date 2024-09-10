abda2b1cfb4952920afc6b6ce168656ff64e6c71
lopikopi <gixxxlileft@gmail.com>
2024-09-10 22:26:23 +0300

    commit

---
+++ main.py
@@ -0,0 +1,97 @@
+# ЗАМЕТКА
+# Есть разные типы данных такие как: int(целые числа) float(не целые числа) string(строка) bool(True or false)
+#and, as, assert, break, class, continue, def, del, elif, else, except, False, finally, for, from
+# , global, if, import, in, is, lambda, None,
+# nonlocal, not, or, pass, raise, return, True, try, while, with, yield
+
+#var1 = len("hello")
+#print(var1)
+
+#x = ("apple", "banana", "cherry")
+#print(x)
+#difference() метод возвращает новый набор без нежелательных элементов -, а difference_update()метод
+# удаляет нежелательные элементы из исходного набора -=.
+
+#x = {"apple", "banana", "cherry"}
+#y = {"google", "microsoft", "apple"}
+#z = x.difference(y)
+#print(z)
+
+#Метод discard()удаляет указанный элемент из набора.
+
+#Метод intersection()возвращает набор, содержащий сходство между двумя или более наборами.Его символ &
+
+#Метод intersection_update()удаляет элементы, которых нет в обоих наборах &=
+
+#Метод isdisjoint()возвращает True, если ни один из элементов не присутствует в обоих наборах
+# , в противном случае он возвращает False.
+
+#Метод symmetric_difference()возвращает набор, содержащий все элементы из обоих наборов, но не элементы, присутствующие
+# в обоих наборах.^
+#Метод symmetric_difference_update()обновляет исходный набор, удаляя элементы, присутствующие в обоих наборах,
+# и вставляя другие элементы^=
+#Метод union()возвращает набор, содержащий все элементы исходного набора и все элементы указанного набора(ов).|
+
+#Метод update()обновляет текущий набор, добавляя элементы из другого набора (или любого другого итерируемого объекта)|=
+#x = 12
+#y = 70 + x
+#print(y)
+
+#a = 1
+#b = 2
+#c = a + b + 16
+#print(c)
+
+#int_number = 3
+#float_number = 3.3
+#complex_number = 3.3 + 2j
+
+#hello_string = "hello"
+#world_string = "world"
+#print(hello_string)
+#print(world_string)
+
+#s = "hello world"
+#print(s)
+
+#s1 = "hello,"
+#s2 = "world!"
+#joined_string = s1 + s2
+#print(joined_string)
+
+#name = "Nikita"
+#my_string = f"hello, {name}!"
+#print(my_string)
+
+# age = 19
+# adult1 = age>= 18
+#
+# age = 15
+# adult2 = age >= 18
+# print(adult1)
+
+# a = 3
+# b = 5
+# c = a > b
+# d = a < b
+# e = a == b
+# f = a != b
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+#a = input("What's your name?: ")
+#print(a)
+
+#age = input("How old are you? ")
+#age = int(age)
+
+# pi = float(3.14)
+# print(pi)
+
+pi_str = str(3.14)
+age_str = str(45)
+print(pi_str)
+print(age_str)
+
